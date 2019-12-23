992c05b022e03c49a76c2cb8902e3c6c0b310176
Oğuzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2019-12-23 19:19:36 +0300

    Math Calculator in C#
    The c # program that mathematically calculates the given arithmetic formula and prints the result to the textbox.

---
+++ MathCalculator/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MathCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.tbxResult = new System.Windows.Forms.TextBox();
+            this.tbxEquation = new System.Windows.Forms.TextBox();
+            this.lblEquation = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(390, 259);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(209, 69);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // tbxResult
+            // 
+            this.tbxResult.Location = new System.Drawing.Point(282, 163);
+            this.tbxResult.Name = "tbxResult";
+            this.tbxResult.Size = new System.Drawing.Size(439, 22);
+            this.tbxResult.TabIndex = 2;
+            // 
+            // tbxEquation
+            // 
+            this.tbxEquation.Location = new System.Drawing.Point(282, 84);
+            this.tbxEquation.Name = "tbxEquation";
+            this.tbxEquation.Size = new System.Drawing.Size(439, 22);
+            this.tbxEquation.TabIndex = 3;
+            // 
+            // lblEquation
+            // 
+            this.lblEquation.AutoSize = true;
+            this.lblEquation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEquation.Location = new System.Drawing.Point(33, 86);
+            this.lblEquation.Name = "lblEquation";
+            this.lblEquation.Size = new System.Drawing.Size(205, 20);
+            this.lblEquation.TabIndex = 4;
+            this.lblEquation.Text = "Matematiksel Denklem:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblResult.Location = new System.Drawing.Point(171, 163);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(67, 20);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Sonuç:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 429);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblEquation);
+            this.Controls.Add(this.tbxEquation);
+            this.Controls.Add(this.tbxResult);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox tbxResult;
+        private System.Windows.Forms.TextBox tbxEquation;
+        private System.Windows.Forms.Label lblEquation;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
